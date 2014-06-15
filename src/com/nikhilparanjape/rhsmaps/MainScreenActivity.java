146e0e1c6b8e5298abaf9aa61bd69c5245512c46
Nikhil Paranjape <nikhil1.paranjape@gmail.com>
2014-06-15 12:18:31 -0400

    New icons

--- src/com/nikhilparanjape/rhsmaps/MainScreenActivity.java
+++ src/com/nikhilparanjape/rhsmaps/MainScreenActivity.java
@@ -1,7 +1,10 @@
 package com.nikhilparanjape.rhsmaps;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Menu;
 
 public class MainScreenActivity extends Activity {
@@ -9,6 +12,10 @@ public class MainScreenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar bar = getActionBar();
+		bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#1E468E")));
+		bar.setDisplayShowHomeEnabled(false);
+		bar.setTitle("RHS Maps"); 
         setContentView(R.layout.activity_main_screen);
     }
 
